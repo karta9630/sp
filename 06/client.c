479fec129af97a15c28a167a0a8be84d7155b931
karta9630 <4909044@gmail.com>
2024-06-07 15:26:59 +0800

    1

---
+++ 06/client.c
@@ -0,0 +1,23 @@
+#include "net.h"
+
+
+int main(int argc, char *argv[]) {
+    char *ip = argv[1];
+    int port = atoi(argv[2]);
+    int sfd = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in saddr, raddr;
+    memset(&saddr, 0, sizeof(saddr));
+    memset(&raddr, 0, sizeof(raddr));
+    saddr.sin_family = AF_INET;
+    saddr.sin_port = htons(port);
+    saddr.sin_addr.s_addr = inet_addr(ip);
+    int rc = connect(sfd, (struct sockaddr*) &saddr, sizeof(struct sockaddr));
+    assert(rc >= 0);
+   
+    while (1) {
+        char msg[SMAX], 
+        fgets(msg, SMAX, stdin);
+    }
+    close(sfd);
+    return 0;   
+}
